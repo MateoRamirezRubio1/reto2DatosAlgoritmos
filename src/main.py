4de95b7dd6c85a3a88c773ecde93e003f43da6d8
Mateo Ramirez Rubio <100296963+MateoRamirezRubio1@users.noreply.github.com>
2022-11-16 22:01:06 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from circuito import *
+
+if __name__ == '__main__':
+    mejoresCaminosBodegas = hamiltoniano(bodegas, centros_comerciales, len(centros_comerciales))
+    ImprimirMejorBodega(mejoresCaminosBodegas)
+    
